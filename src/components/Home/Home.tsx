0913ab73fd8d0bbadd7a15a53aa91da9696a4f4f
frankhn <harfrank2@gmail.com>
2020-06-16 06:46:43 +0200

    add home component

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import './Home.scss';
+
+export interface IHomeProps { }
+
+const Home: React.FC<IHomeProps> = (): React.ReactElement => (
+  <Fragment>
+    {/* <section className="slider"> */}
+      {/* <WelcomeSlider /> */}
+    {/* </section> */}
+  </Fragment>
+);
+
+export default Home
+
