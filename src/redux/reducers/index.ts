b3f38e92cf62eaf6feb737a0f5bce7258328bc1f
frankhn <harfrank2@gmail.com>
2020-06-16 07:31:29 +0200

    remove unused codes

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,7 +1,4 @@
 import { combineReducers } from 'redux';
-// import { authentication, verification, upload, newProduct } from '../../containers/account/store/reducer'
-import { search } from '../../containers/search/store/reducer'
-
 const rootReducer = combineReducers({});
 
 export default rootReducer;
