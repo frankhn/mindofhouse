7867edd4757873ec770b0fe50f20627efe3dee29
frankhn <harfrank2@gmail.com>
2020-06-16 06:42:18 +0200

    check authentication

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import AppRouter from './routes/index';
+import { AppState } from './redux/store/store';
+import { authenticate } from './containers/account/store/actions'
+
+export type IAppProps = {
+  authenticate: any
+};
+
+export interface IAppState { }
+
+class App extends React.Component<IAppProps> {
+
+  componentDidMount() {
+    this.checkAuthenticationStatus()
+  }
+
+  checkAuthenticationStatus = () => {
+    const { authenticate } = this.props;
+    const authKey = localStorage.getItem('authentication_key');
+    return authKey !== null
+      ? authenticate(localStorage.getItem('authentication_key'))
+      : null;
+  };
+
+
+  render() {
+    return (
+        <BrowserRouter>
+          <AppRouter />
+        </BrowserRouter>
+    );
+  }
+}
+
+const mapStateToProps = (state: AppState) => ({});
+
+export default connect(mapStateToProps, { authenticate })(App);
