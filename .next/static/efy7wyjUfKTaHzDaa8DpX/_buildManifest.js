b3766bdf5a84777411b5df2bfed62a8566f0d63f
frankhn <harfrank2@gmail.com>
2020-06-20 23:40:13 +0200

    new build files

---
+++ .next/static/efy7wyjUfKTaHzDaa8DpX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/profile":[a]}}("static\u002Fchunks\u002Fd8b26582f19740fad7e5ebc7b18b68b593ff9599.e1101abe2eaf6f91be2a.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
