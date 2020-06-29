1aa2c9d515df207a9d39a84b4d2627a5c40ac544
frankhn <harfrank2@gmail.com>
2020-06-29 11:12:47 +0200

    apps

---
+++ pages/_app.js
@@ -0,0 +1,55 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "next/app";
+import Head from "next/head";
+import Router from "next/router";
+
+// import "assets/scss/nextjs-material-kit.scss?v=1.1.0";
+import '../assets/scss/nextjs-material-kit.scss'
+
+Router.events.on("routeChangeStart", url => {
+  // console.log(`Loading: ${url}`);
+  document.body.classList.add("body-page-transition");
+  ReactDOM.render(
+    <div></div>,
+    document.getElementById("page-transition")
+  );
+});
+Router.events.on("routeChangeComplete", () => {
+  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
+  document.body.classList.remove("body-page-transition");
+});
+Router.events.on("routeChangeError", () => {
+  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
+  document.body.classList.remove("body-page-transition");
+});
+
+export default class MyApp extends App {
+  componentDidMount() {
+    let comment = document.createComment(`
+
+`);
+    document.insertBefore(comment, document.documentElement);
+  }
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <React.Fragment>
+        <Head>
+          <title>MindOfHouse</title>
+        </Head>
+        <Component {...pageProps} />
+      </React.Fragment>
+    );
+  }
+}
