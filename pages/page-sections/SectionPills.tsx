668a4275f4f3e13d95cacff6374f1f547001b73d
frankhn <harfrank2@gmail.com>
2020-07-09 17:28:50 +0200

    remove unnecessary comments

--- pages/page-sections/SectionPills.tsx
+++ pages/page-sections/SectionPills.tsx
@@ -1,28 +1,11 @@
 import React from "react";
-// import classnames from 'classnames'
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-// import Nouislider from "nouislider-react";
-// import StarRatings from 'react-star-ratings';
-// import {
-//   Container, Row, Col, Card, CardBody, CardTitle,
-//   Form, Label, Input, Nav, NavItem, NavLink, Pagination,
-//   PaginationItem, PaginationLink
-// } from 'reactstrap';
-
-// @material-ui/icons
-// import Dashboard from "@material-ui/icons/Dashboard";
-// import Schedule from "@material-ui/icons/Schedule";
-// import List from "@material-ui/icons/List";
 
 // core components
-import GridContainer from "../../components/Grid/GridContainer";
-// import GridItem from "../../components/Grid/GridItem";
-// import NavPills from "../../components/NavPills/NavPills";
-// import Breadcrumbs from '../../components/Common/Breadcrumb'
+import GridContainer from "../../components/Grid/GridContainer"
 
 import * as styles from '../../assets/jss/nextjs-material-kit/pages/componentsSections/pillsStyle'
-// import Link from "next/link";
 
 const useStyles = makeStyles(styles);
 
@@ -30,14 +13,91 @@ export default function SectionPills() {
 
   const classes: any = useStyles();
   return (
-    <div className={ classes.section } style={ { padding: ".1px 0" } }>
+    <div className={ classes.section } style={{ marginTop: "-7rem"}}>
       <div className={ classes.container }>
         <div id="navigation-pills">
-          <div className={ classes.title }>
-            <h3>Navigation Pills</h3>
-          </div>
           <GridContainer>
-          </GridContainer>
+            <div style={{ zIndex: 1000, background: "#fff", padding: "1rem", borderRadius: ".3rem"}}>
+              <p>
+                Dramatically visualize customer directed convergence
+                without revolutionary ROI. Collaboratively
+                administrate empowered markets via plug-and-play
+                networks. Dynamically procrastinate B2C users after
+                installed base benefits.
+                        </p>
+              <br />
+              <p>
+                Dramatically visualize customer directed convergence
+                without revolutionary ROI. Collaboratively
+                administrate empowered markets via plug-and-play
+                networks. Dynamically procrastinate B2C users after
+                installed base benefits.
+                        </p>
+                        <p>
+                Collaboratively administrate empowered markets via
+                plug-and-play networks. Dynamically procrastinate B2C
+                users after installed base benefits.
+                        </p>
+              <br />
+              <p>
+                Dramatically visualize customer directed convergence
+                without revolutionary ROI. Collaboratively
+                administrate empowered markets via plug-and-play
+                networks. Dynamically procrastinate B2C users after
+                installed base benefits.
+                        </p>
+              <br />
+              <p>
+                Dramatically visualize customer directed convergence
+                without revolutionary ROI. Collaboratively
+                administrate empowered markets via plug-and-play
+                networks. Dynamically procrastinate B2C users after
+                installed base benefits.
+                        </p>
+                        <p>
+                Collaboratively administrate empowered markets via
+                plug-and-play networks. Dynamically procrastinate B2C
+                users after installed base benefits.
+                        </p>
+              <br />
+              <p>
+                Dramatically visualize customer directed convergence
+                without revolutionary ROI. Collaboratively
+                administrate empowered markets via plug-and-play
+                networks. Dynamically procrastinate B2C users after
+                installed base benefits.
+                        </p>
+              <br />
+              <p>
+                Dramatically visualize customer directed convergence
+                without revolutionary ROI. Collaboratively
+                administrate empowered markets via plug-and-play
+                networks. Dynamically procrastinate B2C users after
+                installed base benefits.
+                        </p>
+                        <p>
+                Collaboratively administrate empowered markets via
+                plug-and-play networks. Dynamically procrastinate B2C
+                users after installed base benefits.
+                        </p>
+              <br />
+              <p>
+                Dramatically visualize customer directed convergence
+                without revolutionary ROI. Collaboratively
+                administrate empowered markets via plug-and-play
+                networks. Dynamically procrastinate B2C users after
+                installed base benefits.
+                        </p>
+              <br />
+              <p>
+                Dramatically visualize customer directed convergence
+                without revolutionary ROI. Collaboratively
+                administrate empowered markets via plug-and-play
+                networks. Dynamically procrastinate B2C users after
+                installed base benefits.
+                        </p>
+            </div>
+            </GridContainer>
         </div>
       </div>
     </div>
