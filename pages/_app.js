a310616c9a5ee32b430e6f2f1960417829ccc2e8
frankhn <harfrank2@gmail.com>
2020-07-11 13:56:56 +0200

    import css

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,18 @@ import App from "next/app";
 import Head from "next/head";
 import Router from "next/router";
 
+import 'bootstrap/dist/css/bootstrap.css'
 // import "assets/scss/nextjs-material-kit.scss?v=1.1.0";
 import '../assets/scss/nextjs-material-kit.scss'
+
+// filters css
+import './styles/filters.scss'
+
+// video css
+import './styles/video.scss'
+
+// product
+import './styles/product.scss'
 
 Router.events.on("routeChangeStart", url => {
   // console.log(`Loading: ${url}`);
